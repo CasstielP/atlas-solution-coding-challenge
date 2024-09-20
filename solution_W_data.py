01b2839b4d5b67ecb4232a6bb559dd42ccc6de14
Casstiel <cpss5433@gmail.com>
2024-09-20 09:12:16 -0700

    add code comments

--- solution_W_data.py
+++ solution_W_data.py
@@ -1,21 +1,29 @@
+# import re, regex module from python
 import re
+# import pandas module for display data
 import pandas as pd
 
 file_path = 'w_data.dat'
 
+# open file path and store to var: data
 with open(file_path, 'r') as file:
     data = file.readlines()
 
+
+# declare var lists to append the appropriate regex pattern groups
 day_numbers = []
 max_temps = []
 min_temps= []
 
+# create the regex pattern accordingly
+# groups: date, max temp, min temp
 line_pattern = re.compile(r"\s*(\d+)\s+(\d+)\s+(\d+)")
 
 
+# iterate through var: data and parse each line with line_pattern
 for line in data:
     match = line_pattern.match(line)
-
+# when match found, append each group to the lists accordingly
     if match:
         day = int(match.group(1))
         max_temp = int(match.group(2))
@@ -26,18 +34,17 @@ for line in data:
 
 
 
-        
+# create pandas dataframe to display data
 weather_data = pd.DataFrame({
             'Day': day_numbers,
             'Max Temp': max_temps,
             'Min Temp': min_temps
         })
 
- 
+# create new column to store the temp spread
 weather_data['Temp Spread'] = weather_data['Max Temp'] - weather_data['Min Temp']
 
-
+# declare new var to store the row with min temp spread
 min_spread_row = weather_data.loc[weather_data['Temp Spread'].idxmin()]
 
 print(min_spread_row)
-
--- solution_soccer.py
+++ solution_soccer.py
@@ -1,22 +1,28 @@
+# import re, regex module from python
 import re
+# import pandas module for display data
 import pandas as pd
 
 file_path = 'soccer.dat'
-
+# open file path and store to var: data
 with open(file_path, 'r') as file:
     data = file.readlines()
 
+# declare var lists to append the appropriate regex pattern groups
 team_names = []
 
 scores_for = []
 
 scores_against = []
 
+# create the regex pattern accordingly
+# groups: team name, score for, score agianst
 line_pattern = re.compile(r'\s*\d+\.\s+(\w+[\w_]*?)\s+\d+\s+\d+\s+\d+\s+\d+\s+(\d+)\s+-\s+(\d+)\s+\d+')
 
+# iterate through var: data and parse each line with line_pattern
 for line in data:
     match = line_pattern.match(line)
-
+# when match found, append each group to the lists accordingly
     if match:
         team = match.group(1)
         score_for = int(match.group(2))
@@ -25,14 +31,17 @@ for line in data:
         scores_for.append(score_for)
         scores_against.append(score_against)
 
+# create pandas dataframe to display data
 team_score_data = pd.DataFrame({
         'Team': team_names,
         'Score For': scores_for,
         'Score Against': scores_against
     })
 
+# create new column and calculate the difference
 team_score_data['Score Difference'] = abs(team_score_data['Score For'] - team_score_data['Score Against'])
 
+# declare var to store the row with min score difference
 min_difference_row = team_score_data.loc[team_score_data['Score Difference'].idxmin()]
 
 
